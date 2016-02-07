00db0768343cd0d73a889aa0354cd543880667b9
Travis Hunter <travis.c.hunter@gmail.com>
2016-02-06 19:40:10 -0500

    Fork process per search term

--- twatter.py
+++ twatter.py
@@ -1,11 +1,17 @@
 import os
 import tweepy
 from stream import TwatStreamListener
+from multiprocessing import Process
 
 auth = tweepy.OAuthHandler(os.environ['TWITTER_CONSUMER_KEY'], os.environ['TWITTER_CONSUMER_SECRET'])
 auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], os.environ['TWITTER_ACCESS_SECRET'])
 
-stream_listener = TwatStreamListener('trump')
-twat_stream = tweepy.Stream(auth = auth, listener=stream_listener)
+def stream_twats(search):
+    stream_listener = TwatStreamListener(search)
+    twat_stream = tweepy.Stream(auth = auth, listener=stream_listener)
+    twat_stream.filter(track=[search])
 
-twat_stream.filter(track=['trump'])
+if __name__ == '__main__':
+    for search in ('cat', 'dog'):
+        p = Process(target=stream_twats, args=(search,))
+        p.start()
